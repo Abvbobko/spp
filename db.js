3834e1bc6599047ea35dcd68e21021a65787adcb
Alexei Bobko <41486012+Abvbobko@users.noreply.github.com>
2020-03-08 00:58:35 +0300

    move work woth data base in other file

---
+++ db.js
@@ -0,0 +1,26 @@
+const HOST = "localhost";
+const USER = "root";
+const PASSWORD = "qwerty12345678";
+
+var mysql = require('mysql');
+
+class DataAccessor {
+    _con;
+    constructor(host, user, password) {
+        this._con = mysql.createConnection({
+            host: host,
+            user: user,
+            password: password
+        });
+          
+        this._con.connect(function(err) {
+            if (err) 
+                throw err;
+            console.log("Connected!");
+        });
+    }
+}
+
+var a = 2;
+
+//// сделать все в виде обьекта!!!!!!!!!!!!!
