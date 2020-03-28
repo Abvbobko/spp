14932b3e93d001bf1fcba027cd430c8e02e22b85
Alexei Bobko <abvbobko@gmail.com>
2020-03-29 02:09:14 +0300

    Implement writing data to database

--- src/db.js
+++ src/db.js
@@ -9,7 +9,8 @@ class DataAccessor {
         this._con = mysql.createConnection({
             host: host,
             user: user,
-            password: password
+            password: password,
+            database: "todo_db"
         });
           
         this._con.connect(function(err) {
@@ -20,7 +21,25 @@ class DataAccessor {
     }
 
     insert_task(task_text, task_date, task_status, task_file) {
-        
+        const user = [task_text, task_file.originalname, task_date, 1, task_file.filename];
+        const sql = "INSERT INTO tasks(text, file_name, date, STATUSES_id, name_on_server) VALUES(?, ?, ?, ?, ?)";
+ 
+        this._con.query(sql, user, function(err, results) {
+            if(err) 
+                console.log(err);
+            else 
+                console.log("Данные добавлены");   
+        });
+    }
+
+    close_connection() {
+        // закрытие подключения
+        this._con.end(function(err) {
+            if (err) {
+                console.log("Ошибка: " + err.message);
+            }
+            console.log("Подключение закрыто.");
+        });
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 // подключение express
 const express = require("express");
-var bodyParser = require("body-parser");
 const ejs = require('ejs');
 const multer  = require("multer");
 
 // создаем объект приложения
 const app = express();
-// добавляет возможность доставать переменные из ответа
-//app.use(bodyParser.urlencoded({ extended: true }));
+// добавляет возможность доставать переменные из ответа (и загружать файлы)
 app.use(multer({dest:"../data/files"}).single("file"));
-
 app.set('view engine', 'ejs');
 
 db = require("./db").db;
-
 
 app.get("/", function(request, response){
     // отправляем ответ
@@ -23,24 +19,20 @@ app.get("/", function(request, response){
     
 });
 
-const path = require('path');
 app.post("/", function(request, response){
     // отправляем ответ
     // let status = request.body.status;
+    let text = request.body.task;
+    let date = request.body.date;
     let filedata = request.file;
     let status = request.body.status;
     console.log(status);
     console.log(filedata.originalname);
+    db.insert_task(text, date, status, filedata);
     if(!filedata)
       console.log("Ошибка при загрузке файла");
     else
       console.log("Файл загружен");
-      
-    //console.log('Got body:', request.body);
-    //var fname = request.body.fname;
-    //var lname = request.body.lname;
-    //console.log(fname + " " + lname)
-    //response.send(fname + " " + lname);
 });
 
 // начинаем прослушивать подключения на 3000 порту
