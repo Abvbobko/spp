b5984e05f93b2753f95eb3567de15a3ff0524602
Alexei Bobko <abvbobko@gmail.com>
2020-04-28 18:32:38 +0300

    Add empty methods to server

---
+++ app/server/data_manipulator.js
@@ -0,0 +1,29 @@
+
+class DataManipulator {
+    get_status_map(status_db_table) {
+        let status_map = new Map();
+        status_db_table.forEach(function(status) {
+            status_map.set(status.id, status.name);
+        });
+        return status_map;             
+    }    
+
+    status_id_to_name(tasks, status_map) {        
+        tasks.forEach(task => {            
+            task.status = status_map.get(task.STATUSES_id);
+        });
+        return tasks;
+    }
+
+    get_key_by_value(searched_value, map) {
+        for (let [key, value] of map.entries()) {
+            if (value == searched_value)
+                return key;
+        }        
+        return null;
+    }
+}
+
+manipulator = new DataManipulator();
+
+module.exports = { manipulator };
