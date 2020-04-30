d212883428535d9aff88c4ca5a70d82493fc4f6e
Alexei Bobko <abvbobko@gmail.com>
2020-04-30 19:25:38 +0300

    Delete old code

--- app/server/main.js
+++ app/server/main.js
@@ -99,68 +99,8 @@ app.get("/tasks/:task_id/file", function(request, response) {
   });
 });
 
-// OLD
-
-app.get("/", function(request, response){    
-    db.get_statuses().then(function(statuses) {          
-      // получаем список всех существующих задач в бд
-      db.get_tasks().then(function(tasks) { 
-        let status_map = data_manipulator.get_status_map(statuses);
-        let status_names = Array.from(status_map.values());
-        tasks = manipulator.status_id_to_name(tasks, status_map);        
-        response.render("main.ejs", 
-          {"statuses": status_names, 
-          "selected_status_name": "",
-          "tasks": tasks});
-      });
-    }).catch((err) => {console.log(err)})
-});
-
-app.post("/", function(request, response){
-    // отправляем ответ
-    // let status = request.body.status;
-    let text = request.body.task;
-    let date = request.body.date;
-    let filedata = request.file;
-    let status = request.body.status;   
-    if(!filedata)
-      console.log("Не было передано файлов");
-    else
-      console.log("Файл загружен");
-
-    db.insert_task(text, date, status, filedata).then(function() {
-      response.redirect("/");
-    });
-});
-
-app.post("/filter", function(request, response){
-  let status_name;
-  // проблема: не видит request.body
-  console.log(`status:${request.body.status}`);
-  if (request.body.status == "all") {
-    response.redirect("/");
-  } else {
-    status_name = request.body.status;
-    db.get_statuses().then(function(statuses) {          
-      let status_map = data_manipulator.get_status_map(statuses);  
-      db.get_tasks(status_name).then(function(tasks) {         
-        let status_names = Array.from(status_map.values());        
-        tasks = manipulator.status_id_to_name(tasks, status_map);                
-        response.render("main.ejs", 
-          {"statuses": status_names, 
-          "selected_status_name": status_name, 
-          "tasks": tasks
-        });
-      });
-    }).catch((err) => {console.log(err)})
-  }  
-});
-
-app.post("/delete", function(request, response) {  
-  db.delete_task(request.body.task_id).then(function() {
-    response.redirect("/");
-  });
-});
+// app.get("/", function(request, response){    
+// });
 
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
